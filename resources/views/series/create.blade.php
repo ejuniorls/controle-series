51d774b87d23e6a99fe2a31a9c821054447e0391
ejuniorls <ejuniorls@outlook.com>
2022-02-27 16:07:30 -0300

    Alterando views para blade.php

---
+++ resources/views/series/create.blade.php
@@ -0,0 +1,17 @@
+@extends('layout')
+
+@section('cabecalho')Adicionar Série @endsection
+
+@section('conteudo')
+    <section class="container pt-3">
+        <form method="post">
+            <div class="mb-3">
+                <label for="nome" class="form-label">Nome</label>
+                <input type="text" class="form-control" id="nome" aria-describedby="nome"
+                       placeholder="Digite o nome da série">
+            </div>
+
+            <button type="submit" class="btn-sm btn-dark">Enviar</button>
+        </form>
+    </section>
+@endsection

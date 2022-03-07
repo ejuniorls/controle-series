c3887e7ffd8b8c3e8566246077fd5da415008c6e
ejuniorls <ejuniorls@outlook.com>
2022-03-06 21:34:08 -0300

    Criando relacionamentos do banco de dados

--- app/Serie.php
+++ app/Serie.php
@@ -7,8 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Serie extends Model
 {
     protected $table = 'series';
-     protected $fillable = [
-         'nome'
-     ];
+    protected $fillable = [
+        'nome'
+    ];
+
+    public function temporadas()
+    {
+        return $this->hasMany(Temporada::class);
+    }
 
 }

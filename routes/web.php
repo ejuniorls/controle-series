647b2dc0a7aa351658e5db8eeb4dfca61998c1b6
ejuniorls <ejuniorls@outlook.com>
2022-03-22 23:26:08 -0300

    Impementado autenticação na aplicação

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,39 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/series', 'SeriesController@index')->name('series.index');
-Route::get('/series/criar', 'SeriesController@create')->name('series.create');
-Route::post('/series/criar', 'SeriesController@store')->name('series.store');
-Route::delete('/series/{id}', 'SeriesController@destroy')->name('series.delete');
-Route::post('/series/{id}/editaNome', 'SeriesController@put')->name('series.put');
 
-Route::get('/series/{serieId}/temporadas', 'TemporadasController@index')->name('temporadas.index');
-Route::get('/temporadas/{temporada}/episodios', 'EpisodiosController@index')->name('episodios.index');
-Route::post('/temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir')->name('episodios.assistir');
+Route::group(['prefix' => 'series'], function () {
+    Route::get('/', 'SeriesController@index')->name('series.index');
+    Route::get('/{serieId}/temporadas', 'TemporadasController@index')->name('temporadas.index');
+
+    Route::group(['middleware' => 'autenticador'], function () {
+        Route::get('/criar', 'SeriesController@create')->name('series.create');
+        Route::post('/criar', 'SeriesController@store')->name('series.store');
+        Route::delete('/{id}', 'SeriesController@destroy')->name('series.delete');
+        Route::post('/{id}/editaNome', 'SeriesController@put')->name('series.put');
+    });
+});
+
+Route::group(['prefix' => 'temporadas'], function () {
+    Route::get('/{temporada}/episodios', 'EpisodiosController@index')->name('episodios.index');
+
+    Route::group(['middleware' => 'autenticador'], function () {
+        Route::post('/{temporada}/episodios/assistir', 'EpisodiosController@assistir')->name('episodios.assistir');
+    });
+});
+
+
+Route::get('/entrar', 'EntrarController@index')->name('entrar.index');
+Route::post('/entrar', 'EntrarController@entrar')->name('entrar.entrar');
+
+Route::get('/registrar', 'RegistroController@create')->name('registro.create');
+Route::post('/registrar', 'RegistroController@store')->name('registro.store');
+
+Route::get('/sair', function () {
+    Auth::logout();
+    return redirect('/entrar');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
